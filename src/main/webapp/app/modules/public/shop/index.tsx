f22ced72837c0dc335ff538ef2600affc70000b6
QuatTT <quattt@mirabo-global.com>
2025-09-28 23:59:01 +0700

    Disable test

--- src/main/webapp/app/modules/public/shop/index.tsx
+++ src/main/webapp/app/modules/public/shop/index.tsx
@@ -2,21 +2,22 @@ import ProductImages from 'app/shared/components/ecommerce/product/product-image
 import ProductInfo from 'app/shared/components/ecommerce/product/product-info';
 import ProductTabs from 'app/shared/components/ecommerce/product/product-tab';
 import RelatedProducts from 'app/shared/components/ecommerce/product/related-product';
+import { ShopSidebar, ProductList } from 'app/shared/components/ecommerce/shop';
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
 export const Shop = () => {
   return (
-    <section className="product-details spad">
+    <section className="shop spad">
       <Container>
-        <Row>
-          <ProductImages />
-          <ProductInfo />
-          <ProductTabs />
-        </Row>
-        <Row>
-          <RelatedProducts />
-        </Row>
+        <div className="row">
+          <div className="col-lg-3 col-md-3">
+            <ShopSidebar />
+          </div>
+          <div className="col-lg-9 col-md-9">
+            <ProductList />
+          </div>
+        </div>
       </Container>
     </section>
   );
